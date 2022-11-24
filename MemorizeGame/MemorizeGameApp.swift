65aa9d1975ae9752b2845806da71c4691437dd50
rodrigofrancisco <rodrigo.francisco@wizeline.com>
2022-11-24 10:19:14 -0600

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Rodrigo Francisco on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
