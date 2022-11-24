341a190cd3a1d8b5271f3c677f8c6160d0ed7867
rodrigofrancisco <rodrigo.francisco@wizeline.com>
2022-11-24 12:05:42 -0600

    Add logic for memorize game

--- MemorizeGame/Model/MemoryGame.swift
+++ MemorizeGame/Model/MemoryGame.swift
@@ -5,8 +5,10 @@
 //  Created by Rodrigo Francisco on 24/11/22.
 //
 
-struct MemoryGame<CardContent> {
-    var cards: [Card]
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    private(set) var cards: [Card]
+    
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
     
     struct Card: Identifiable {
         var isFaceUp: Bool = false
@@ -26,7 +28,22 @@ struct MemoryGame<CardContent> {
     }
     
     mutating func choose(card: Card) {
-        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+            !cards[chosenIndex].isFaceUp,
+            !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            }
+            else {
+               for index in cards.indices {
+                   cards[index].isFaceUp = false
+               }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+           }
             cards[chosenIndex].isFaceUp.toggle()
         }
     }
